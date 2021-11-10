2e1b1e898f482746661a14444add15df8d20ad29
POXS44 <vzeledon7@gmail.com>
2021-11-10 11:52:37 -0600

    Test de guardar producto

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -18,24 +18,35 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
-    public function test_delete()
+    // public function test_delete()
+    // {
+    //     $response = $this->delete('/api/productos/2');
+
+    //     $response
+    //         ->assertStatus(200)
+    //         ->assertJson([
+    //             'mensaje' => 'productos Eliminado exitosamente',
+    //         ]);
+    // }
+    // public function test_update()
+    // {
+    //     $response = $this->put('/api/productos/4', ['nombre' => 'acuaman','tipo' => 'tulita']);
+
+    //     $response
+    //         ->assertStatus(200)
+    //         ->assertJson([
+    //             'mensaje' => 'productos actualizado exitosamente',
+    //         ]);
+    // }
+
+    public function test_guardar()
     {
-        $response = $this->delete('/api/productos/2');
+        $response = $this->post('/api/productos', ['nombre' => 'PHONE9','tipo' => 'Celular']);
 
         $response
             ->assertStatus(200)
             ->assertJson([
-                'mensaje' => 'productos Eliminado exitosamente',
-            ]);
-    }
-    public function test_update()
-    {
-        $response = $this->put('/api/productos/4', ['nombre' => 'acuaman','tipo' => 'tulita']);
-
-        $response
-            ->assertStatus(200)
-            ->assertJson([
-                'mensaje' => 'productos actualizado exitosamente',
+                'mensaje' => 'Productos guardado exitosamente',
             ]);
     }
 }
